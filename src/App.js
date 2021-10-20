3575b0f84cf8f893449e2fb417a59b6555544239
Andre Pato <anpato1994@gmail.com>
2021-10-20 15:06:49 -0400

    update starter code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
-
+import Button from './components/Button'
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Button text="Awesome Button" />
+    </div>
+  )
 }
 
 export default App
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-function Button() {
-  return <button>Click Me</button>
+function Button(props) {
+  console.log(props)
+  return <button>{props.text}</button>
 }
 
 export default Button
